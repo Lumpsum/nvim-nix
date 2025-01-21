c0db213345ad5e5a112ee31dffed0205b1100b3b
Rick Vergunst <vergunstje@hotmail.com>
2025-01-21 23:05:14 +0100

    Update

--- lua/conf/lsp/always.lua
+++ lua/conf/lsp/always.lua
@@ -85,8 +85,8 @@ require("lze").load {
 
                         draw = {
                             columns = {
-                                { "label", "label_description", gap = 1 },
-                                { "kind_icon", gap = 1, "kind" }
+                                { "label",     "label_description", gap = 1 },
+                                { "kind_icon", gap = 1,             "kind" }
                             },
                             treesitter = { "lsp" }
                         }
@@ -123,6 +123,50 @@ require("lze").load {
                 settings = {
                     Lua = {
                         hints = { enabled = true },
+                    }
+                }
+            })
+
+            lspconfig.gopls.setup({
+                on_attach = function(_, bufnr)
+                    vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
+
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        pattern = "*.go",
+                        callback = function()
+                            local params = vim.lsp.util.make_range_params()
+                            params.context = { only = { "source.organizeImports" } }
+
+                            local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                            for cid, res in pairs(result or {}) do
+                                for _, r in pairs(res.result or {}) do
+                                    if r.edit then
+                                        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                                        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                                    end
+                                end
+                            end
+                            vim.lsp.buf.format({ async = false })
+                        end
+                    })
+                end,
+                cmd = { "gopls" },
+                settings = {
+                    gopls = {
+                        completeUnimported = true,
+                        usePlaceholders = true,
+                        analyses = {
+                            unusedparams = true,
+                        },
+                        hints = {
+                            assignVariableTypes = true,
+                            compositeLiteralFields = true,
+                            compositeLiteralTypes = true,
+                            constantValues = true,
+                            functionTypeParameters = true,
+                            parameterNames = true,
+                            rangeVariableTypes = true,
+                        }
                     }
                 }
             })
