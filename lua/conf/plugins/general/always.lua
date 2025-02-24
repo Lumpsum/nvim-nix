72f09a8f0da1df3ca7dbbd2325e614e44e5a9b81
Rick Vergunst <R39969@eon.com>
2025-02-24 09:31:50 +0100

    Add bigfile config

--- lua/conf/plugins/general/always.lua
+++ lua/conf/plugins/general/always.lua
@@ -1,6 +1,34 @@
 local colorschemeName = nixCats("colorscheme")
 
 vim.g.loaded_netrwPlugin = 1
+vim.g.bigfile_size = 1024 * 1024 * 0.2
+
+local function augroup(name)
+    return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
+vim.filetype.add({
+    pattern = {
+        [".*"] = {
+            function(path, buf)
+                return vim.bo[buf].filetype ~= "bigfile" and path and vim.fn.getfsize(path) > vim.g.bigfile_size and
+                    "bigfile"
+                    or nil
+            end,
+        },
+    },
+})
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    group = augroup("bigfile"),
+    pattern = "bigfile",
+    callback = function(ev)
+        vim.b.minianimate_disable = true
+        vim.schedule(function()
+            vim.bo[ev.buf].syntax = vim.filetype.match({ buf = ev.buf }) or ""
+        end)
+    end,
+})
+
 require("oil").setup({
     default_file_explorer = true,
     columns = {
